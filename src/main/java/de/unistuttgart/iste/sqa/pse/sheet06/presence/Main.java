04215048b4c5df88bf8e3aa37d67a8dfdd68e975
avlmg <avluismi@gmail.com>
2023-11-29 14:17:39 +0100

    presen

--- src/main/java/de/unistuttgart/iste/sqa/pse/sheet06/presence/Main.java
+++ src/main/java/de/unistuttgart/iste/sqa/pse/sheet06/presence/Main.java
@@ -3,6 +3,7 @@ package de.unistuttgart.iste.sqa.pse.sheet06.presence;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Implement here
+		//MyImmutableStudent Sarah = new MyImmutableStudent("Sarah", 12345, );
+		//System.out.println(Sarah.getStudentName()+", "+Sarah.getMatrikelNummer());
 	}
 }
--- src/main/java/de/unistuttgart/iste/sqa/pse/sheet06/presence/MyImmutableStudent.java
+++ src/main/java/de/unistuttgart/iste/sqa/pse/sheet06/presence/MyImmutableStudent.java
@@ -7,4 +7,27 @@ package de.unistuttgart.iste.sqa.pse.sheet06.presence;
  * @author (your name)
  * @version (a version number or a date)
  */
-public class MyImmutableStudent {}
+public final class MyImmutableStudent {
+    private final String StudentName;
+    private final long MatrikelNummer;
+    private final Address Geburtsort;
+
+    public MyImmutableStudent(String studentName, long matrikelNummer, Address geburtsort) {
+        StudentName = studentName;
+        MatrikelNummer = matrikelNummer;
+        Geburtsort = new Address(geburtsort.getStreetname(),geburtsort.getStreetnumber(),); //nicht fertig
+    }
+
+    public String getStudentName() {
+        return StudentName;
+    }
+
+    public long getMatrikelNummer() {
+        return MatrikelNummer;
+    }
+// muss auch geander werden
+    public Address getGeburtsort() {
+        return Geburtsort;
+    }
+
+}

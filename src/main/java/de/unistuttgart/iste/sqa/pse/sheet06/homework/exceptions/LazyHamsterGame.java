7fe14521849ff9949a56b2c0f0e6fd20b8ea7871
avlmg <avluismi@gmail.com>
2023-12-01 21:13:19 +0100

    Update LazyHamsterGame.java

--- src/main/java/de/unistuttgart/iste/sqa/pse/sheet06/homework/exceptions/LazyHamsterGame.java
+++ src/main/java/de/unistuttgart/iste/sqa/pse/sheet06/homework/exceptions/LazyHamsterGame.java
@@ -29,17 +29,16 @@ public class LazyHamsterGame extends SimpleHamsterGame {
 		moveMultipleSteps(5);
 	}
 
-	// TODO add documentation and contracts here
 	/*@
-	@ requires
-	@ ensures
-	@ throws TooLazyException
+	@ requires Hamster is initialised.
 	 */
 	/**
+	 * Tries to move the Hamster forward.
 	 *
+	 * @throws TooLazyException If the hamster is too lazy to move.
+	 * @throws NoWayToGoException If there is no way to go.
 	 */
 	public void tryToMove() {
-		// TODO Implement here.
 		double randomValue = Math.random();
 		if (randomValue < 0.7) {
 			if (paule.frontIsClear()) {
@@ -54,18 +53,15 @@ public class LazyHamsterGame extends SimpleHamsterGame {
 
 	}
 	/*@
-	@ requires
-	@ ensures
-	@ throws TooLazyException
+	@ requires Hamster is initialised
+	@ ensures Hamster moves the specified number of steps.
 	 */
 	/**
-	 *
+	 * Ensures paule move the number of given step animating it if it is too lazy.
+	 * @param numberOfSteps The number of steps to move.
+	 * @throws TooLazyException If the hamster is too lazy to move.
 	 */
-	// TODO add documentation and contracts here
 	public void moveMultipleSteps(int numberOfSteps) {
-		// TODO Implement here.
-
-
 		for (int i = 0; i < numberOfSteps; i++) {
 			try {
 				tryToMove();
@@ -79,14 +75,19 @@ public class LazyHamsterGame extends SimpleHamsterGame {
 				if (isCaged()) {
 					break;
 				}
-
 			}
 		}
 	}
-
-	// TODO add documentation and contracts here
-	public boolean isCaged() {
-		// TODO Implement here.
+	
+	/*@
+	@ requires Hamster is placed in the game.
+	@ ensures boolean value of if the hamster is caged
+	 */
+	/**
+	 * Checks if the hamster is caged.
+	 *
+	 * @return True if the hamster is trapped; otherwise, false.
+	 */	public boolean isCaged() {
 		Direction originalDirection = paule.getDirection();
 		while (!paule.frontIsClear()) {
 			paule.turnLeft();
